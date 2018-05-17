e0bffd679cb4c6a4035b1592e01237a553a3a7ed
lee <233liyu@gmail.com>
2018-05-17 22:23:34 +0800

    data formatting first upload

--- HelloTensorflow.py
+++ HelloTensorflow.py
@@ -2,3 +2,6 @@ import tensorflow as tf
 hello = tf.constant('Hello, TensorFlow!')
 sess = tf.Session()
 print(sess.run(hello))
+
+pl = "1231222222222222233333333333333333333333"
+print(pl[:50])
---
+++ ReadFiles.py
@@ -0,0 +1,138 @@
+import os
+import csv
+from collections import Counter
+
+# data set dir
+root_dir = '/Users/lee/Desktop/大四/毕业设计/GP_data'
+ndpi_label_filename = 'ndpi_result.txt'
+single_packet_length = 1500
+
+
+# return sub dirs in the root dic
+def sub_dir_list():
+    for root, dirs, files in os.walk(root_dir):
+        if root == root_dir:
+            return dirs
+
+
+# read from the raw data file and return a joint payload
+# @return: [return payload, payload_length, process, success]
+def read_raw_data(file_name):
+    payload = ""
+    process = "NULL"
+    p_count = 0
+    try:
+        with open(file_name) as f:
+            content = f.readlines()
+    except IOError:
+        print("file= ", file_name, "is not found")
+        return '', process, p_count, False
+    else:
+        content = [x.strip() for x in content]
+        read_type = 0
+        for line in content:
+            if read_type == 1:
+                # joint the payload as one str
+                payload += line.strip()
+                read_type = 0
+                continue
+            if read_type == 2:
+                # save the process info
+                process = line[:line.find(' ')]
+                read_type = 0
+                continue
+
+            if not line.find('src:'):
+                read_type = 1
+                p_count += 1
+                # count the total length of the flow
+                # payload_length += int(line[line.find("payload length:") + len("payload length:"):])
+                continue
+            if not line.find('COMMAND'):
+                read_type = 2
+                continue
+        return payload, process, p_count, True
+
+
+# read from the ndpi result data
+# @return: list of tuples: flow name, Ms_protocol, app_protocol
+def read_ndpi_result(ndpi_file_name):
+    with open(ndpi_file_name) as f:
+        content = f.readlines()
+    # get rid of the \n
+    content = [x.strip() for x in content]
+    container = []
+    # split into tuple
+    for item in content:
+        container.append(item.split('\t'))
+    return container
+
+
+def write_payload_to_csv(date_info, flow_name, payload):
+    csv_file_name = os.path.join(root_dir, 'payload_info.csv')
+    pl = payload
+    data = []
+    # cut the packet into single_packet_length
+    while True:
+        info_tuple = (str(date_info + ':' + flow_name), pl[:single_packet_length])
+        data.append(info_tuple)
+        if len(pl) < single_packet_length:
+            break
+        else:
+            pl = pl[single_packet_length:]
+
+    with open(csv_file_name, "a") as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        for info in data:
+            writer.writerow(info)
+
+
+def write_date_ndpi_to_csv(date, ndpi_result_list):
+    csv_file_name = os.path.join(root_dir, 'ndpi_result.csv')
+    with open(csv_file_name, "a") as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        for flow_name, ms_pro, app_pro, process in ndpi_result_list:
+            writer.writerow((str(date + ':' + flow_name), ms_pro, app_pro, process))
+
+
+print("start to format data")
+for sub_dir in sub_dir_list():
+    print("entering dir: ", sub_dir)
+    file_dir = os.path.join(root_dir, sub_dir)
+    ndpi_file = os.path.join(file_dir, ndpi_label_filename)
+    ndpi_result = read_ndpi_result(ndpi_file)
+
+    error_file = []
+    com_result = []
+    pro_result = []
+    count_result = Counter()
+
+    payload_length = 0
+    packet_count = 0
+    for flow, ms_p, app_p in ndpi_result:
+        loads, process_name, count, result = read_raw_data(os.path.join(file_dir, flow))
+        if result:
+            write_payload_to_csv(sub_dir, flow, loads)
+            payload_length += len(loads)
+            packet_count += count
+            com_result.append((flow, ms_p, app_p, process_name))
+
+            if (ms_p, app_p) not in pro_result:
+                pro_result.append((ms_p, app_p))
+            for (ms_p, app_p) in pro_result:
+                count_result[(ms_p, app_p)] += 1
+        else:
+            # mark the file-missing item
+            error_file.append(flow)
+
+    print("date ", sub_dir, ": payload data writing finished")
+    write_date_ndpi_to_csv(sub_dir, com_result)
+    print("date ", sub_dir, ": label writing finished")
+    print("\t total packet: ", packet_count, ", total bytes:", payload_length, "bytes")
+    print("\t total session counted: ", len(com_result))
+    print("\t total protocol counted: ", len(count_result))
+    print("\t ", count_result)
+    print("\t missing total ", len(error_file), " files:")
+    for err_info in error_file:
+        print("\t\t", err_info)
+    print("---------------------------------------------------------")
